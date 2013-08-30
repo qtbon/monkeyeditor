30ace087acde519f8fa23628bd38330e75ca5b6b
Shawn Adams <shawn.henry.adams@gmail.com>
2013-08-30 11:15:49 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MonkeyEditor::Application.config.secret_token = 'ff34820258aa7aafbae0e0ac4b1d904ab2d07c3f980133ad126d589b6f772c17b37a1ebc58dd82719e5e5413e2f05dee66ebda64332b9ee0d066619d0d9e0cad'
